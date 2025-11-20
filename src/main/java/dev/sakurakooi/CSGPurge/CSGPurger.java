bd75a2d3f4d63cd470a694d3f07f997b159bce45
SakuraKooi <me@sakurakooi.dev>
2025-11-20 12:58:57 +0800

    fix: incorrectly removed group

--- src/main/java/dev/sakurakooi/CSGPurge/CSGPurger.java
+++ src/main/java/dev/sakurakooi/CSGPurge/CSGPurger.java
@@ -6,6 +6,7 @@ import com.alibaba.fastjson.JSONArray;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
@@ -29,12 +30,14 @@ public class CSGPurger {
                 if (children.childrens.isEmpty()) {
                     ast.removeChild(children);
                     anythingCleaned.incrementAndGet();
+                    continue;
                 }
             }
             if (collapsableTag.contains(children.name)) {
-                if (children.childrens.size() == 1) {
+                if (children.childrens.size() == 1 && checkGroupHasOnlyOneObject(children)) {
                     ast.replaceAt(children, children.childrens.get(0));
                     anythingCleaned.incrementAndGet();
+                    continue;
                 }
             }
             if ("multmatrix".equalsIgnoreCase(children.name)) {
@@ -47,6 +50,7 @@ public class CSGPurger {
                         firstChild.arguments = matrix2Str(newMatrix);
                         ast.replaceAt(children, firstChild);
                         anythingCleaned.incrementAndGet();
+                        continue;
                     }
                 }
             }
@@ -60,6 +64,44 @@ public class CSGPurger {
         }
         ast.childrens.forEach(ast1 -> purge(ast1, anythingCleaned));
         return ast;
+    }
+
+    private static final List<String> realObjectList = Arrays.asList(
+            "group",
+            "union",
+            "difference",
+            "intersection",
+            // 2d
+            "circle",
+            "square",
+            "polygon",
+            "text",
+            "projection",
+            // 3d
+            "sphere",
+            "cube",
+            "cylinder",
+            "polyhedron",
+            "linear_extrude",
+            "rotate_extrude",
+            "surface"
+    );
+
+    private static boolean checkGroupHasOnlyOneObject(CSGParser.CSGAst children) {
+        int found = 0;
+        for (CSGParser.CSGAst csgAst : children.childrens) {
+            if (Objects.equals(csgAst.name, "multmatrix")) {
+                if (checkGroupHasOnlyOneObject(csgAst))
+                    found++;
+            }
+            if (realObjectList.contains(csgAst.name)) {
+                found++;
+            }
+
+            if (found > 1)
+                return false;
+        }
+        return found == 1;
     }
 
     public static double[][] json2Matrix(JSONArray array) {
